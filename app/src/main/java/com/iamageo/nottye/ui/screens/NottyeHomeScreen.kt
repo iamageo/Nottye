1f418374b02396cbf85b73f1d8a8669d3130bbd8
iamageo <geovani.af4@gmail.com>
2022-08-31 14:34:10 -0300

    adding filter section + refactor + fix errors

--- app/src/main/java/com/iamageo/nottye/ui/screens/NottyeHomeScreen.kt
+++ app/src/main/java/com/iamageo/nottye/ui/screens/NottyeHomeScreen.kt
@@ -1,5 +1,6 @@
 package com.iamageo.nottye.ui.screens
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -8,11 +9,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.iamageo.domain.util.NoteOrder
+import com.iamageo.domain.util.OrderType
 import com.iamageo.nottye.R
+import com.iamageo.nottye.ui.screens.components.OrderSection
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun NottyeHomeScreen(
     navController: NavController
@@ -58,6 +64,21 @@ fun NottyeHomeScreen(
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
+            AnimatedVisibility(
+                visible = true,
+                enter = fadeIn() + slideInVertically(),
+                exit = fadeOut() + slideOutVertically()
+            ) {
+                OrderSection(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp),
+                    noteOrder = NoteOrder.Date(OrderType.Ascending),
+                    onOrderChange = {
+                        //viewModel.onEvent(NotesEvent.Order(it))
+                    }
+                )
+            }
         }
     }
 }
--- domain/src/main/java/com/iamageo/domain/repository/NottyeRepository.kt
+++ domain/src/main/java/com/iamageo/domain/repository/NottyeRepository.kt
@@ -3,6 +3,7 @@ package com.iamageo.domain.repository
 import com.iamageo.domain.model.Nottye
 import kotlinx.coroutines.flow.Flow
 
+
 interface NottyeRepository {
 
     fun getAllNottyes() : Flow<List<Nottye>>
