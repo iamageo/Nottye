98d32957e85e350da1153c292bea21c9bc40144d
iamageo <geovani.af4@gmail.com>
2022-09-10 14:37:18 -0300

    creating AppModule for database, repository and use cases

---
+++ app/src/main/java/com/iamageo/nottye/di/AppModule.kt
@@ -0,0 +1,46 @@
+package com.iamageo.nottye.di
+
+import android.app.Application
+import androidx.room.Room
+import com.iamageo.data.datasource.NottyeDatabase
+import com.iamageo.data.repository.NottyeRepositoryImpl
+import com.iamageo.domain.repository.NottyeRepository
+import com.iamageo.domain.usecases.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNottyeDatabase(app: Application): NottyeDatabase {
+        return Room.databaseBuilder(
+            app,
+            NottyeDatabase::class.java,
+            NottyeDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNottyeRepository(db: NottyeDatabase): NottyeRepository {
+        return NottyeRepositoryImpl(db.nottyeDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNottyeUseCases(repository: NottyeRepository): NottyeUseCases {
+        return NottyeUseCases(
+            getNottyes = GetNottyes(repository),
+            deleteNottye = DeleteNottye(repository),
+            addNottye = AddNottye(repository),
+            getNottye = GetNottye(repository)
+        )
+    }
+
+}
